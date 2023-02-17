4f58a0c928340da3e991b97ee1172092354abd4c
mahdee18 <mahdeec10@gmail.com>
2023-02-18 00:38:16 +0600

    Rectangle Card Commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Common Function
+function getInputValue(inputId) {
+    const inputField = document.getElementById(inputId)
+    const inputFieldString = inputField.value;
+    const inputFieldNumber = parseFloat(inputFieldString)
+    return inputFieldNumber;
+}
+//Get Triangle Area
+document.getElementById('btn-triangle').addEventListener('click', function () {
+    const triangleBase = getInputValue('tri-base')
+    const triangleHeight = getInputValue('tri-height')
+    const triangleArea = 0.5 * triangleBase * triangleHeight;
+    console.log(triangleArea);
+}) 
