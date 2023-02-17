335bd5f7bed9039b3a3820091530c6f74a2b53d7
mahdee18 <mahdeec10@gmail.com>
2023-02-18 02:46:45 +0600

    All Cards Commited

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ function getInputValue(inputId) {
     const inputFieldNumber = parseFloat(inputFieldString)
     return inputFieldNumber;
 }
+function getInputText(textId) {
+    const textField = document.getElementById(textId)
+    const textFieldString = textField.innerText;
+    const textFieldNumber = parseFloat(textFieldString)
+    return textFieldNumber;
+}
 // OutPut Field for common Function
 function getOutputValue(outputId, value){
     const outputField = document.getElementById(outputId)
@@ -16,7 +22,7 @@ document.getElementById('btn-triangle').addEventListener('click', function () {
     const triangleHeight = getInputValue('tri-height')
     const triangleArea = 0.5 * triangleBase * triangleHeight;
     const triangleAreaWithComment = 'Triangle Area is :' + triangleArea;
-    const outputField = getOutputValue('output',triangleAreaWithComment)
+    const outputField = getOutputValue('output-triangle',triangleAreaWithComment)
 
 })
 
@@ -26,9 +32,26 @@ document.getElementById('btn-rectangle').addEventListener('click', function () {
     const rectangleLength = getInputValue('rec-length')
     const rectangleArea = rectangleWidth * rectangleLength;
     const rectangleAreaWithComment = 'Rectangle Area is :' + rectangleArea;
-    const outputField = getOutputValue('output',rectangleAreaWithComment)
+    const outputField = getOutputValue('output-rectangle',rectangleAreaWithComment)
 })
 
+// Get Parallelogram Area
+document.getElementById('btn-parallelogram').addEventListener('click',function(){
+    const parallelogramBase = getInputText('para-base')
+    const parallelogramHeight = getInputText('para-height')
+    const parallelogramArea = parallelogramBase*parallelogramHeight;
+    const parallelogramAreaWithComment = 'Parallelogram Area is :'+ ' ' + parallelogramArea;
+    const outputField = getOutputValue('output-parallelogram',parallelogramAreaWithComment)
+
+})
+// Get Rhombus Area
+document.getElementById('btn-rhombus').addEventListener('click',function(){
+    const firstDiagonal = getInputText('first-diagonal')
+    const secondDiagonal = getInputText('second-diagonal')
+    const rhombusArea =0.5 * firstDiagonal*secondDiagonal;
+    const rhombusAreaWithComment =  'Rhombus Area is :'+ ' ' + rhombusArea;
+    const outputField = getOutputValue('output-rhombus',rhombusAreaWithComment)
+})
 
 
 
