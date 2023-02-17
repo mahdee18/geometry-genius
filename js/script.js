d60f6cf1c81e0c8b1feaa8f203480dd8b3d8c4bd
mahdee18 <mahdeec10@gmail.com>
2023-02-18 01:38:53 +0600

    Parallelogram Card Commit

--- js/script.js
+++ js/script.js
@@ -5,17 +5,29 @@ function getInputValue(inputId) {
     const inputFieldNumber = parseFloat(inputFieldString)
     return inputFieldNumber;
 }
-
+// OutPut Field for common Function
+function getOutputValue(outputId, value){
+    const outputField = document.getElementById(outputId)
+    outputField.innerText = value;
+}
 //Get Triangle Area
 document.getElementById('btn-triangle').addEventListener('click', function () {
     const triangleBase = getInputValue('tri-base')
     const triangleHeight = getInputValue('tri-height')
     const triangleArea = 0.5 * triangleBase * triangleHeight;
-    const triangleAreaWithComment = 'The Triangle Area is :'+triangleArea;
-    const outputField = document.getElementById('output')
-    outputField.innerText = triangleAreaWithComment;
-}) 
+    const triangleAreaWithComment = 'Triangle Area is :' + triangleArea;
+    const outputField = getOutputValue('output',triangleAreaWithComment)
 
+})
+
+// Get Rectangle Area
+document.getElementById('btn-rectangle').addEventListener('click', function () {
+    const rectangleWidth = getInputValue('rec-width')
+    const rectangleLength = getInputValue('rec-length')
+    const rectangleArea = rectangleWidth * rectangleLength;
+    const rectangleAreaWithComment = 'Rectangle Area is :' + rectangleArea;
+    const outputField = getOutputValue('output',rectangleAreaWithComment)
+})
 
 
 
