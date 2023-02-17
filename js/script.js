cc595f3d3eaa03b78f099d7602eff8c745695045
mahdee18 <mahdeec10@gmail.com>
2023-02-18 00:59:44 +0600

    Parallelogram Card Commit

--- js/script.js
+++ js/script.js
@@ -5,10 +5,18 @@ function getInputValue(inputId) {
     const inputFieldNumber = parseFloat(inputFieldString)
     return inputFieldNumber;
 }
+
 //Get Triangle Area
 document.getElementById('btn-triangle').addEventListener('click', function () {
     const triangleBase = getInputValue('tri-base')
     const triangleHeight = getInputValue('tri-height')
     const triangleArea = 0.5 * triangleBase * triangleHeight;
-    console.log(triangleArea);
+    const triangleAreaWithComment = 'The Triangle Area is :'+triangleArea;
+    const outputField = document.getElementById('output')
+    outputField.innerText = triangleAreaWithComment;
 }) 
+
+
+
+
+
